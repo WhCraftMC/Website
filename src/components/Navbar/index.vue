2a34b295d289a80be6a6af35f54b732c379f5901
Rumble <109781840+BiliRumble@users.noreply.github.com>
2023-11-16 12:04:06 +0000

    feat: wtf?

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -10,14 +10,14 @@
       <!-- 顶部栏 -->
 			<nav class="header-nav">
 				<ul>
-          <button class="hamburger-btn">
+          <!--<button class="hamburger-btn">
             <div v-if="true" style="color: white; text-align: center;">
               <h2 style="padding-left: 5px;"><img src="https://skin.fxcraft.cn/avatar/0?size=36" />bilirumble</h2>
             </div>
             <div style="color: white" v-if="false">
               <h2><i class="mdui-icon material-icons">&#xe853;</i> 请登录</h2>
             </div>
-          </button>
+          </button>-->
 
           <!-- 开关菜单按钮 -->
 					<button class="hamburger-btn" @click="toggle">
