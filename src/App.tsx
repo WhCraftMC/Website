3913ac7acdee520b2a254f13ee01c75743303592
Rumble <109781840+BiliRumble@users.noreply.github.com>
2024-03-12 11:22:21 +0000

    feat: 重构项目

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return {
+        <div style={{ color:"blue" }}>
+            <h1>hello!</h1>
+        </div>
+    }
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    root: process.cwd(),
+
+    server: {
+        port: 3000,
+        open: " ",
+    },
+
+    build: {
+        outDir: "",
+        assetsDir: "",
+    },
+
+    plugins: [react()],
+});
