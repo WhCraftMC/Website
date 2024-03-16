fea9e55ccc635ac861de6b9400cedff14de6ebf2
Rumble <109781840+BiliRumble@users.noreply.github.com>
2024-03-16 03:19:41 +0000

    feat: 迁移(4/10)

--- src/routes/Punish/utils/TextTools.ts
+++ src/routes/Punish/utils/TextTools.ts
@@ -1,14 +1,12 @@
 export function RawText(str: string) {
-    //const newStr = str.replace(/&([a-zA-Z]+)([klmnor])?|&(\d+)/g, '');
-    //return newStr;
-    return str;
+    const newStr = str.replace(/&([a-zA-Z]+)([klmnor])?|&(\d+)/g, '');
+    return newStr;
 }
 
 export function setLength(str: string, length: number) {
-    //const maxLength = length;
-    //const newStr = str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
-    //return newStr;
-    return str;
+    const maxLength = length;
+    const newStr = str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
+    return newStr;
 }
 
 export function formattedTime(timestamp: number, untime: boolean) {
